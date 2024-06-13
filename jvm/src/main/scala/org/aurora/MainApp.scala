f0d0523b8298be4c361ac3e80d1d4bbe1c715dc4
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-06-12 21:10:00 -0400

    added CORS customization

--- jvm/src/main/scala/org/aurora/MainApp.scala
+++ jvm/src/main/scala/org/aurora/MainApp.scala
@@ -3,32 +3,44 @@ package org.aurora
 import zio._
 import zio.http._
 import zio.http.endpoint._
+import zio.http.Middleware.{CorsConfig, cors}
+import zio.http.Header.{AccessControlAllowOrigin, Origin}
 object MainApp extends ZIOAppDefault :
 
   import org.aurora._, dataimport._
-  importpatients
+  //TODO MAYBE NOT NEEDED
+  val config: CorsConfig =
+  CorsConfig(
+    allowedOrigin = {  //allows origin from vite server to access routes on server
+      case origin if origin == Origin.parse("http://localhost:5173").toOption.get =>
+        Some(AccessControlAllowOrigin.Specific(origin))
+      case _                                                                      => None
+    },
+  )
+
 
   val routes =
     Routes(
-      Method.GET / Root -> handler(Response.text("Greetings at your service!!! Arnoldorferdorf!!!")),
+      Method.GET / Root -> handler(Response.text("Greetings at your service!!! Arnoldorferdorf!!!xxxxx")),
       Method.GET / "greet" -> handler { (req: Request) =>
         val name = req.queryParamToOrElse("name", "World")
         Response.text(s"Hello $name!")
         },
-        Method.GET / "patients"   -> handler {
-          import org.aurora._, dataimport._
-          Response.text(s"$importpatients")
-        },
-        Method.GET / "patientsjson"   -> handler {
-          import org.aurora._, dataimport._
-          import org.aurora.shared._, dto._
-          import zio.json._
-          Response.text(s"${importpatients.toJson}")
-        }
+      Method.GET / "patients"   -> handler {
+        import org.aurora._, dataimport._
+        Response.text(s"$importpatients")
+      },
+      Method.GET / "patientsjson"   -> handler {
+        import org.aurora._, dataimport._
+        import org.aurora.shared._, dto._
+        import zio.json._
+        Response.text(s"${importpatients.toJson}")
+      }
+    )  @@ cors(config) //cors configuration
 
-    )
+  val staticroutes =  Routes.empty @@ Middleware.serveResources(Path.empty / "static") 
   override val run = {
     Console.printLine("please visit http://localhost:8080") 
-    Server.serve(routes).provide(Server.default)
+    Server.serve(routes++staticroutes).provide(Server.default)
 }
 
