96a71291484c6db21c62cea662f516575d9dcc0a
AuroraConst <auroraconstellations@gmail.com>
2024-10-22 20:31:50 -0400

    update

--- jvm/src/main/scala/com/axiom/MainApp.scala
+++ jvm/src/main/scala/com/axiom/MainApp.scala
@@ -9,11 +9,12 @@ object MainApp extends ZIOAppDefault :
 
   import com.axiom._, dataimport.api._
   //TODO MAYBE NOT NEEDED
+  val validSites = List("http://localhost:5173", "http://localhost:8080", "vscode-webview://03js7hav97p76rvju1vgp6e739m8h196h6ic3dcg5268kb1h5e8e").map(Origin.parse(_).toOption.get)
   val config: CorsConfig =
   CorsConfig(
     allowedOrigin = {  //allows origin from vite server to access routes on server
-      case origin if origin == Origin.parse("http://localhost:5173").toOption.get =>
-        Some(AccessControlAllowOrigin.Specific(origin))
+      case origin if validSites.filter(_ == origin).size > 0 =>
+        Some(AccessControlAllowOrigin.Specific(origin)) 
       case _  => None
     },
   )
@@ -36,11 +37,11 @@ object MainApp extends ZIOAppDefault :
         import zio.json._
         Response.text(s"${importpatients.toJson}")
       }
-    )  @@ cors(config) //cors configuration
+    )  @@ cors(config) //cors configuration. 
 
   val staticroutes =  Routes.empty @@ Middleware.serveResources(Path.empty / "static") //accesses resources within directories and subdirectories
   override val run = {
-    Console.printLine("please visit http://localhost:8080") 
+    Console.printLine("please visit http://localhost:8080")  
     Server.serve(routes++staticroutes).provide(Server.default)
 }
 
